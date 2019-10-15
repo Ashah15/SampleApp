725c77f532fa5899a1a43e05c479dc09053b1f19
Marvellous Michaels <marvellousubani@gmail.com>
2019-10-15 23:09:01 +0100

    Finish static pages

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+	root 'static_pages#home'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
@@ -7,7 +9,5 @@ Rails.application.routes.draw do
   get  'static_pages/about'
 
   get  'static_pages/contact'
-
-	root 'application#home'
   
 end
