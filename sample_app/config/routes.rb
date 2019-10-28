028158249bce2e2b8e4c671d20c99a59f218cc77
Marvellous Ubani <marvellousubani@gmail.com>
2019-10-28 20:22:54 +0100

    Revert "Completed Chapter 5"

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-
 	root 'static_pages#home'
 
-  get '/home', to: 'static_pages#home'
+  get 'static_pages/home'
 
-  get '/help', to: 'static_pages#help'
+  get 'static_pages/help'
 
-  get  '/about', to: 'static_pages#about'
+  get  'static_pages/about'
 
-  get  '/contact', to: 'static_pages#contact'
-
-  get  '/signup',  to: 'users#new'
+  get  'static_pages/contact'
   
 end
--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -7,31 +7,31 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get root" do
-  	get root_path
+  	get root_url
   	assert_response :success
   end
 
   test "should get home" do
-    get home_path
+    get static_pages_home_url
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
-    get help_path
+    get static_pages_help_url
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
-    get about_path
+    get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
 
 
   test "should get contact" do
-    get contact_path
+    get static_pages_contact_url
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
